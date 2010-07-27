82045d90ab4a0861bfdae95a6fa04874aaa8909d
Timur Vafin <timur.vafin@flatsoft.com>
2010-07-27 19:47:31 +0400

    Fixup default app steps

--- features/step_definitions/app_steps.rb
+++ features/step_definitions/app_steps.rb
@@ -3,7 +3,7 @@ Given /^I am not authenticated$/ do
 end
 
 Given /^I am logged in as "(.*?)\/(.*?)"$/ do |email, password|
-  Given %{a user: "admin" exists with email: "#{email}", password: "#{password}", admin: true}
+  Given %{a user: "current_user" exists with email: "#{email}", password: "#{password}"}
   And %{I go to the new user session page}
   And %{I fill in "user_email" with "#{email}"}
   And %{I fill in "user_password" with "#{password}"}
