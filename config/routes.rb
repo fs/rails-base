bb9a8f58b8951b1f943debfddea32b424ba46574
Timur Vafin <timur.vafin@flatsoft.com>
2010-09-17 15:48:59 +0400

    Fixup deprication warnings for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails3Base::Application.routes.draw do
-  devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout' }
-  match 'login', :to => 'devise/sessions#new', :as => 'new_user_session'
-  match 'logout', :to  => 'devise/sessions#destroy', :as => 'destroy_user_session'
-  match 'signup', :to => 'devise/registrations#new', :as => 'new_user_registration'
+  devise_for :users
 
+  as :user do
+    get 'login', :to => 'devise/sessions#new', :as => 'new_user_session'
+    get 'logout', :to  => 'devise/sessions#destroy', :as => 'destroy_user_session'
+    get 'signup', :to => 'devise/registrations#new', :as => 'new_user_registration'
+  end
+  
   root :to => 'dashboard#index'
 end
