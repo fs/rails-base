5b7f5848e49a99180c92a56e6f8699f654598c02
Timur Vafin <timur.vafin@flatsoft.com>
2010-10-12 18:52:49 +0400

    [#13] Update time formats

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,6 +1,9 @@
 {
-  :short_date  => "%x",              # 04/13/10
-  :long_date   => "%a, %b %d, %Y"   # Tue, Apr 13, 2010
+  :short_date => '%x',
+  :long_date  => '%a, %b %d, %Y',
+  :us         => '%m/%d/%Y %I:%M %p',
+  :us_date    => '%m/%d/%Y',
+  :us_time    => '%I:%M %p'
 }.each do |k, v|
   Time::DATE_FORMATS.update(k => v)
 end
