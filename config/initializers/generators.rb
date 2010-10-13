f5580e4c6737404b6158112dd52c933544a499fe
Timur Vafin <timur.vafin@flatsoft.com>
2010-10-13 06:49:37 +0400

    Cleanup generators configuration

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -4,11 +4,6 @@ Rails3Base::Application.configure do
     g.fixture false
     g.helper false
     g.integration_tool false
-
-    g.test_framework  :rspec
-    g.controller_specs false
-    g.helper_specs false
-    g.routing_specs false
-    g.view_specs false
+    g.test_framework  :rspec, :controller_specs => false, :helper_specs => false, :routing_specs => false, :view_specs => false
   end
 end
