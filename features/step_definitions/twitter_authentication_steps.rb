fd551665b60861f162fc17d75104d02008d1e7b9
Timur Vafin <timur.vafin@flatsoft.com>
2010-12-03 19:14:43 +0300

    [#6651567] Refactored sign in with twitter feature

--- features/step_definitions/twitter_authentication_steps.rb
+++ features/step_definitions/twitter_authentication_steps.rb
@@ -1,17 +1,18 @@
 Given /^I have identity for Twitter authorization$/ do
   @identity = Factory.create(:twitter_identity, :user => @current_user)
-  token_requests = {:oauth_token => 'fake', :oauth_token_secret => 'fake', :user_id => @identity.uid}.to_query
-
-  stub_request(:any, 'https://api.twitter.com/oauth/request_token').
-      to_return(:body => token_requests)
-
-  stub_request(:any, 'https://api.twitter.com/oauth/access_token').
-      to_return(:body => token_requests)
-
-  stub_request(:any, 'https://api.twitter.com/1/account/verify_credentials.json').
-      to_return(:body => {:id => @identity.uid}.to_json)
 end
 
-When /^Twitter authorizes me$/ do
+Given /^the Twitter OAuth request is successful$/ do
+  stub_request(:any, 'https://api.twitter.com/oauth/request_token').
+      to_return(:body => {:oauth_token => 'fake', :oauth_token_secret => 'fake'}.to_query)
+end
+
+When /^Twitter authorizes me as registered user$/ do
+  stub_request(:any, 'https://api.twitter.com/oauth/access_token').
+      to_return(:body => {:oauth_token => 'fake', :oauth_token_secret => 'fake', :user_id => 1}.to_query)
+
+  stub_request(:any, 'https://api.twitter.com/1/account/verify_credentials.json').
+      to_return(:body => {:id => 1, :screen_name => 'smith', :name => 'John Smith'}.to_json)
+
   visit(user_omniauth_callback_path(:twitter, :oauth_token => 'fake', :oauth_verifier => 'verifier'))
 end
