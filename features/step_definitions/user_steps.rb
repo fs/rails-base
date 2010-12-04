a0763669d120b1655057bf9237f9b477de4c052f
Timur Vafin <timur.vafin@flatsoft.com>
2010-12-04 03:35:35 +0300

    Implemented account update with out password cucumber feature

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -57,6 +57,15 @@ When /^I submit my user information$/ do
   click_button 'user_submit'
 end
 
+When /^I submit my user information without current password$/ do
+  fill_in 'user_full_name', :with => @current_user.full_name
+  fill_in 'user_email', :with => @current_user.email
+  fill_in 'user_password', :with => '123456'
+  fill_in 'user_password_confirmation', :with => '123456'
+
+  click_button 'user_submit'
+end
+
 When /^I submit my user information with invalid current password$/ do
   fill_in 'user_current_password', :with => 'invalid current password'
   click_button 'user_submit'
