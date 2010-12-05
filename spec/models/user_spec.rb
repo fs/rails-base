25fd15eb2567ed77bd6994e34c50fcfda17852e8
Timur Vafin <timur.vafin@flatsoft.com>
2010-12-06 00:40:08 +0300

    Move omniauth spec to the specific spec file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,44 +7,4 @@ describe User do
   it { should allow_mass_assignment_of(:password_confirmation) }
 
   it { should validate_presence_of :full_name }
-
-  context 'class methods:' do
-    context '#find_by_identity_for' do
-      context 'when current_user given' do
-        let(:current_user) { Factory :confirmed_user }
-
-        context 'identity exists for given provider and uid' do
-          let(:identity) { Factory :twitter_identity, :user => current_user }
-          let(:method) { lambda { User.find_by_identity_for(identity.provider, identity.uid, current_user) } }
-          before { identity }
-
-          it 'should return current user' do
-            method.call.should eql current_user
-          end
-
-          it 'should not create new identity' do
-            method.should_not change(UserIdentity, :count)
-          end
-        end
-
-        context 'identity does not exists for given provider and uid' do
-          let(:method) { lambda { User.find_by_identity_for('twitter', '1', current_user) } }
-
-          it 'should return current user' do
-            method.call.should eql current_user
-          end
-
-          it 'should create new identity' do
-            method.should change(current_user.identities, :count).by(1)
-          end
-
-          context 'new identity' do
-            let(:identity) { method.call.identities.first }
-            it { identity.provider.should eql 'twitter' }
-            it { identity.uid.should eql '1' }
-          end
-        end
-      end
-    end
-  end
 end
