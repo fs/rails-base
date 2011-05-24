f00a764fa515f0d3ae794794ea8546febede69e9
Iskander Haziev <g.valmon@gmail.com>
2011-05-24 03:49:36 -0700

    formtastic replaced by simple_form

--- features/step_definitions/common/app_steps.rb
+++ features/step_definitions/common/app_steps.rb
@@ -6,5 +6,7 @@ When /^I send "(GET|POST|PUT|DELETE)" request to the (.+) page(?: with #{capture
 end
 
 Then /^I should see "([^"]*)" error for "([^"]*)"$/ do |error_message, id|
-  Then %Q{I should see "#{error_message}" within "li##{id}_input p.inline-errors"}
+  with_scope %Q{".input:has(##{id}) span.error"} do
+    Then %Q{I should see "#{error_message}"}
+  end
 end
