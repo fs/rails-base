6aa2037fd65e4c5a48c4610738cc03fd03b30509
Timur Vafin <timur.vafin@flatsoft.com>
2011-12-12 20:08:06 +0400

    Mixup cucumber warnings

--- features/step_definitions/common/app_steps.rb
+++ features/step_definitions/common/app_steps.rb
@@ -1,12 +1,5 @@
-When /^I send "(GET|POST|PUT|DELETE)" request to the (.+) page(?: with #{capture_fields}(?: namespaced with "([^"]*)")?)?$/ do |method, page_name, fields, namespace|
-  params = parse_fields(fields) || {}
-  params = {namespace => params} if namespace.present?
-
-  Capybara.current_session.driver.process(method.downcase.to_sym, path_to("the #{page_name} page"), params)
-end
-
 Then /^I should see "([^"]*)" error for "([^"]*)"$/ do |error_message, id|
   with_scope %Q{".input:has(##{id}) span.error"} do
-    Then %Q{I should see "#{error_message}"}
+    step %Q{I should see "#{error_message}"}
   end
 end
