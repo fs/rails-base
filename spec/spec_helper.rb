bae38fac7b0a051c3ea050527b70a924f09a36f6
Timur Vafin <timur.vafin@flatsoft.com>
2012-01-22 03:41:58 +0400

    Use ruby 1.9.3 Replace rcov with simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+# Run code coverage tool on top
+require 'simplecov'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
