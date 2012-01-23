b376fc2faf53609d333fafda4b6bd3ca6d494553
Timur Vafin <timur.vafin@flatsoft.com>
2012-01-23 15:07:26 +0400

    Add commented ActionMailer settings to initializers/mailer.rb for Heroku's Cedar stack

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,1 +1,14 @@
 ActionMailer::Base.default_url_options[:host] = configatron.host
+
+# Heroku specific setting for Cedar stack http://devcenter.heroku.com/articles/sendgrid#usage
+# heroku addons:add sendgrid:starter
+#
+# ActionMailer::Base.smtp_settings = {
+#   :address        => 'smtp.sendgrid.net',
+#   :port           => '587',
+#   :authentication => :plain,
+#   :user_name      => ENV['SENDGRID_USERNAME'],
+#   :password       => ENV['SENDGRID_PASSWORD'],
+#   :domain         => 'heroku.com'
+# }
+# ActionMailer::Base.delivery_method = :smtp
