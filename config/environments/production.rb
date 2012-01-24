09f357b9610626587066c02a9a90cc59373f964a
Timur Vafin <timur.vafin@flatsoft.com>
2012-01-24 19:30:23 +0400

    Move mailer config for heroku to the production env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,14 @@ Rails3Base::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Heroku specific setting for Cedar stack http://devcenter.heroku.com/articles/sendgrid#usage
+  # config.action_mailer.smtp_settings = {
+  #   :address        => 'smtp.sendgrid.net',
+  #   :port           => '587',
+  #   :authentication => :plain,
+  #   :user_name      => ENV['SENDGRID_USERNAME'],
+  #   :password       => ENV['SENDGRID_PASSWORD'],
+  #   :domain         => 'heroku.com'
+  # }
 end
