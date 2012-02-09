6ec606300124f0ff7b8dffef7329b655c4e1c552
Rustem Giniyatullin <rustem.giniyatullin@flatsoft.com>
2012-02-09 13:59:03 +0400

    User navigation dropdown menu

--- config/navigations/user_navigation.rb
+++ config/navigations/user_navigation.rb
@@ -1,14 +1,19 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.autogenerate_item_ids = false
-  navigation.selected_class = nil
-  navigation.active_leaf_class = 'active'
 
   navigation.items do |user|
-    user.dom_class = 'user nav'
+    user.dom_class = 'pull-right nav '
 
     if user_signed_in?
-      user.item :account, current_user.full_name_with_email, edit_user_registration_path
-      user.item :sign_out, 'Sign out', destroy_user_session_path
+      user.item :account, current_user.full_name, '#', 
+                :class => 'dropdown',
+                :link => {:class => 'dropdown-toggle', 'data-toggle' => 'dropdown'} do |account|
+                  
+        account.dom_class = 'dropdown-menu'
+        
+        account.item :edit_account, 'Edit profile', edit_user_registration_path
+        account.item :sign_out, 'Sign out', destroy_user_session_path
+      end
     else
       user.item :sign_in, 'Sign in', new_user_session_path
       user.item :sign_up, 'Sign up', new_user_registration_path
