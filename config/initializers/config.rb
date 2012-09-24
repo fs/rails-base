7e100e6c0519a70f60ee73bef9b2eee146efdad7
Timur Vafin <timur.vafin@flatstack.com>
2012-09-24 22:04:43 +0400

    Remove old config.rb

--- config/initializers/config.rb
+++
@@ -1,7 +0,0 @@
-# The shortcut for getting application config
-# Application config should be stored in the config/application.rb, config/environments/*.rb
-module Kernel
-  def app_config
-    Rails.application.config
-  end
-end
