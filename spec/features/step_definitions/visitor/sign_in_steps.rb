6e8fd18489ea1e230124164a3d4ba3004e98086e
Timur Vafin <timur.vafin@flatstack.com>
2012-11-28 19:04:22 +0400

    Fill in fields in features by element id after capybara changed this behaviour

--- spec/features/step_definitions/visitor/sign_in_steps.rb
+++ spec/features/step_definitions/visitor/sign_in_steps.rb
@@ -1,8 +1,8 @@
 def sign_in_with(email, password)
   visit new_user_session_path
 
-  fill_in "Email", with: email
-  fill_in "Password", with: password
+  fill_in "user_email", with: email
+  fill_in "user_password", with: password
 
   click_button "Sign in"
 end
