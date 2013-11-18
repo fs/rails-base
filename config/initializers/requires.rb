307361704f83ecc4f879507de3bf2b29cae59148
Timur Vafin <timur.vafin@flatstack.com>
2013-11-18 17:59:34 +0400

    Fix {} style

--- config/initializers/requires.rb
+++ config/initializers/requires.rb
@@ -1,5 +1,5 @@
 # Require recursively all files in the lib/extensions
-Dir[Rails.root.join('lib/extensions/**/*.rb')].each {|f| require f}
+Dir[Rails.root.join('lib/extensions/**/*.rb')].each { |f| require(f) }
 
 # Require only files placed in the lib folder
-Dir[Rails.root.join('lib/*.rb')].each {|f| require f}
+Dir[Rails.root.join('lib/*.rb')].each { |f| require(f) }
