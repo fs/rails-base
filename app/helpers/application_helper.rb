eea5a56c9e72411ee3ea8b35ce9c4e0073bc7d5d
Timur Vafin <timur.vafin@flatstack.com>
2013-11-19 14:12:14 +0400

    Use attr_reader instead of attr_accessor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  attr_accessor :show_title
+  attr_reader :show_title
   alias_method :show_title?, :show_title
 
   def title(page_title, show_title = true)
-    self.show_title = show_title
+    @show_title = show_title
     content_for(:title) { page_title.to_s }
   end
 end
