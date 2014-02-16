afcb4e40a755596575f4452da563b0e5721d752f
Alisa Muzafarova <alisa.muzafarova@flatstack.com>
2014-02-16 12:30:20 +0400

    [#124] update locales, add hints for forms

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -45,7 +45,7 @@ SimpleForm.setup do |config|
     b.use :error, wrap_with: { tag: :span, class: :error }
   end
 
-  config.wrappers :foundation, class: :input, hint_class: :field_with_hint, error_class: :error do |b|
+  config.wrappers :foundation, class: :input, hint_class: :has_hint, error_class: :error do |b|
     b.use :html5
     b.use :placeholder
     b.use :label
@@ -56,14 +56,14 @@ SimpleForm.setup do |config|
     b.optional :readonly
 
     b.use :input
-    b.use :error, wrap_with: { tag: :small }
+    b.use :error, wrap_with: { tag: :small, class: :error }
 
     # Uncomment the following line to enable hints. The line is commented out by default since Foundation
     # does't provide styles for hints. You will need to provide your own CSS styles for hints.
-    # b.use :hint,  wrap_with: { tag: :span, class: :hint }
+    b.use :hint,  wrap_with: { tag: :small, class: :hint }
   end
 
-  config.wrappers :with_labels, class: :row, hint_class: :field_with_hint, error_class: :error do |b|
+  config.wrappers :with_labels, class: :row, hint_class: :has_hint, error_class: :error do |b|
     b.use :html5
     b.use :placeholder
 
