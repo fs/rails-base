c9a1bc6c488ba2900ffd7eea0e08a115f11f9cde
des1tro <makcimka1994@gmail.com>
2014-06-27 09:53:25 +0400

    removed goto page object, till it iss unused

---
+++ spec/support/pages/routing_pages/go_to_page.rb
@@ -0,0 +1,28 @@
+# class GoToPage
+#   include Capybara::DSL
+
+#   def sign_in
+#     visit 'users/sign_in'
+#   end
+
+#   def edit_page
+#     visit 'users/edit'
+#   end
+
+#   def sign_up
+#     visit('users/sign_up')
+#   end
+
+#   def index_page
+#     visit '/'
+#   end
+
+#   def forgot_password_page
+#     visit('users/password/new')
+#   end
+
+#   def resend_confirmation_page
+#     visit('users/confirmation/new')
+#   end
+
+# end
