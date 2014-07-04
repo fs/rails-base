1e674446bd7e973080c49941e1d7e7bff1665dec
Vasily Polovnyov <vasily@polovnyov.ru>
2014-07-04 14:55:09 +0400

    correct Bundler.require in config/application.rb: :default, Rails.env

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module RailsBase
   class Application < Rails::Application
