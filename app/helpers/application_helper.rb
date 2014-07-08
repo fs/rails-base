d0993b390818d84d3963d1e4404a3850b893fd0d
IlnurNasyrov <ilnur.nasyrov@flatstack.com>
2014-07-08 16:01:21 +0400

    Remove old helper title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  attr_reader :show_title
-  alias_method :show_title?, :show_title
-
-  def title(page_title, show_title = false)
-    @show_title = show_title
-    content_for(:title) { page_title.to_s }
-  end
 end
