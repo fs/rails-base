76d3a655b7068fc6ef269936950988a35bbe2f68
Airat Shigapov <airat.shigapov@flatstack.com>
2014-07-09 12:29:33 +0400

    Cleanup sections load order in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,8 +9,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 
-Dir[Rails.root.join('spec/support/sections/*.rb')].each { |f| require f }
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/{sections,**}/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
