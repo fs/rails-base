491ecd8853514b3c300d5d5d3ceca154b179dfa0
Ildar Kayumov <ildar.kayumov@flatstack.com>
2014-07-10 16:58:01 +0400

    Move time formats from initializer to en.yml file

--- config/initializers/time_formats.rb
+++
@@ -1,9 +0,0 @@
-{
-  short_date: '%x',
-  long_date: '%a, %b %d, %Y',
-  us: '%m/%d/%Y %I:%M %p',
-  us_date: '%m/%d/%Y',
-  us_time: '%I:%M %p'
-}.each do |k, v|
-  Time::DATE_FORMATS.update(k => v)
-end
