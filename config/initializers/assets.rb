85e97a45aaca10761eedb090bb8d3a2c7351f7bd
Vasily Polovnyov <vasily@polovnyov.ru>
2014-12-22 20:26:39 +0300

    drop IE8 support

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,10 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(
-  vendor/modernizr.js
-  ie8/nwmatcher-1.2.5-min.js
-  ie8/selectivizr-1.0.3b.js
-  ie8/respond.min.js
-  ie8/rem.js
-)
+Rails.application.config.assets.precompile += %w( vendor/modernizr.js )
