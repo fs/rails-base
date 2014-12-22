8723bf7f93debb9636b4abd50c8c4df41f0809f9
Vasily Polovnyov <vasily@polovnyov.ru>
2014-12-22 20:19:29 +0300

    properly setup database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,7 +12,7 @@ end
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
 
   config.include EmailSpec::Helpers
