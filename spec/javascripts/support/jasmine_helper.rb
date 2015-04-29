e8b0fe53b5ad0c2ea84ba2c5b23c2feb29016bc8
Vasily Polovnyov <vasily@polovnyov.ru>
2015-04-29 20:05:35 +0300

    do not autoinstall phantomjs in CI environment

--- spec/javascripts/support/jasmine_helper.rb
+++ spec/javascripts/support/jasmine_helper.rb
@@ -8,8 +8,7 @@
 #    config.boot_files = lambda { ['/absolute/path/to/boot_dir/file.js'] }
 # end
 #
-# Example: prevent PhantomJS auto install, uses PhantomJS already on your path.
-# Jasmine.configure do |config|
-#    config.prevent_phantom_js_auto_install = true
-# end
-#
+# Prevent PhantomJS auto install, uses PhantomJS already on your path.
+Jasmine.configure do |config|
+  config.prevent_phantom_js_auto_install = ENV['CI'].present?
+end
