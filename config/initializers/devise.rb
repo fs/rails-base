5ff5ba4f25820c6afbcee5ddaf34c0cdd1d9b8f7
Timur Vafin <timur.vafin@flatstack.com>
2015-05-06 13:02:48 +0300

    Use custom devise mailer UserMailer
    * mailer views moved from devise/mailer to user_mailer
    * custom mailer will allow us to wrap @resource with presenter to use in the mailer views

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   config.mailer_sender = app_config.noreply
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'UserMailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
