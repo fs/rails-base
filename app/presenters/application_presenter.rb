818d7c4138102d8e92e0f62f9761354ab4f2e8b5
Timur Vafin <timur.vafin@flatstack.com>
2015-05-11 13:33:13 +0300

    User private attr_reader for record and collection instead of instance variables

--- app/presenters/application_presenter.rb
+++ app/presenters/application_presenter.rb
@@ -1,17 +1,20 @@
 class ApplicationPresenter
   include Enumerable
 
+  attr_reader :record, :collection
+  private :record, :collection
+
   # active_model
   delegate :to_model, :to_key, :to_param, :to_partial_path,
-    to: :@record
+    to: :record
 
   # partial rendering
   delegate :to_ary,
-    to: :@collection, allow_nil: true
+    to: :collection, allow_nil: true
 
   # pagination
   delegate :current_page, :total_pages, :limit_value, :entry_name, :total_count, :offset_value, :last_page?,
-    to: :@collection
+    to: :collection
 
   def initialize(record_or_collection)
     if record_or_collection.respond_to?(:each)
@@ -22,6 +25,6 @@ class ApplicationPresenter
   end
 
   def each
-    @collection.each { |item| yield(new(item)) }
+    collection.each { |item| yield(new(item)) }
   end
 end
--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,9 +1,9 @@
 class UserPresenter < ApplicationPresenter
   # Devise compability
-  delegate :is_a?, to: :@record
-  delegate :email, :full_name, to: :@record
+  delegate :is_a?, to: :record
+  delegate :email, :full_name, to: :record
 
   def full_name_with_email
-    "#{@record.full_name} (#{@record.email})"
+    "#{record.full_name} (#{record.email})"
   end
 end
