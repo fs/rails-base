3f4a36d5ab604c8d1d6888f6b3da45031957a6a8
Vasily Polovnyov <vasily@polovnyov.ru>
2015-05-29 20:32:21 +0300

    reorganize visitor specs:
    * clean up
    * wordsmithing: remove useless "successfully"
    * extract "Password Reset" and "Resend Confirmation Email" specs

---
+++ spec/features/visitor/password_reset_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+feature "Sign In" do
+  let(:user) { create :user, :confirmed }
+
+  scenario "User resets his password" do
+    visit new_user_password_path
+
+    fill_in "Enter your email address", with: user.email
+    click_button "Send me reset password instructions"
+
+    open_email(user.email)
+
+    expect(current_email).to have_subject "Reset password instructions"
+    expect(current_email).to have_body_text(user.full_name)
+  end
+end
