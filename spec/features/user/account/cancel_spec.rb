335db299b46a75187753f59b39af2b6d53b362cf
Vasily Polovnyov <vasily@polovnyov.ru>
2015-06-03 19:51:59 +0300

    reorganize user feature specs:
    * use word "User" in scenario descriptions when we assume that user is authenticated
    * use word "Visitor" for scenarios with anonymous users
    * clean up user specs and unify `feature` titles

--- spec/features/user/account/cancel_spec.rb
+++ spec/features/user/account/cancel_spec.rb
@@ -1,21 +1,22 @@
 require "rails_helper"
 
-feature "Cancel account" do
+feature "Cancel Account" do
+  let(:user_password) { "123456" }
   let(:user) { create :user, :confirmed }
 
-  before(:each) do
-    sign_in(user.email, "123456")
+  before do
+    sign_in(user.email, user_password)
+    visit edit_user_registration_path(user)
   end
 
-  scenario "I cancel my account" do
-    visit edit_user_registration_path(user)
+  scenario "User cancels account" do
     click_link "Cancel my account"
 
     expect(page).to have_content("Sign in")
     expect(page).to have_content("Bye! Your account has been successfully cancelled. We hope to see you again soon.")
 
     visit new_user_session_path
-    sign_in(user.email, "123456")
+    sign_in(user.email, user_password)
 
     expect(page).to have_content("Invalid email or password.")
   end
--- spec/features/visitor/sign_in_spec.rb
+++ spec/features/visitor/sign_in_spec.rb
@@ -4,20 +4,20 @@ feature "Sign In" do
   let(:user) { create :user, :confirmed }
   let(:unconfirmed_user) { create :user, :not_confirmed }
 
-  scenario "User signs in with valid credentials" do
+  scenario "Visitor signs in with valid credentials" do
     sign_in(user.email, "123456")
 
     expect(page).to have_content("Sign out")
   end
 
-  scenario "User signs in with invalid credentials" do
+  scenario "Visitor signs in with invalid credentials" do
     sign_in(user.email, "wrong password")
 
     expect(page).to have_content("Sign in")
     expect(page).to have_content("Invalid email or password")
   end
 
-  scenario "User signs in with unconfirmed email address" do
+  scenario "Visitor signs in with unconfirmed email address" do
     sign_in(unconfirmed_user.email, "123456")
 
     expect(page).to have_content("You have to confirm your email address before continuing.")
