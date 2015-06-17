713034e016bb63c6006e029ae8a960626d877257
Vasily Polovnyov <vasily@polovnyov.ru>
2015-06-17 17:09:04 +0300

    address code review:
    * use `background` instead of `before` in feature specs
    * use `registered_user` var in sign up spec
    * use appropriate description in password reset feature

--- spec/features/visitor/sign_up_spec.rb
+++ spec/features/visitor/sign_up_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 feature "Sign Up" do
   let(:user_attributes) { attributes_for(:user).slice(:full_name, :email, :password, :password_confirmation) }
-  let(:user) { User.find_by_email(user_attributes[:email]) }
+  let(:registered_user) { User.find_by_email(user_attributes[:email]) }
 
-  before(:each) do
+  background do
     visit new_user_registration_path
 
     fill_form(:user, user_attributes)
@@ -13,17 +13,17 @@ feature "Sign Up" do
   end
 
   scenario "Visitor signs up" do
-    open_email(user.email)
+    open_email(registered_user.email)
 
     expect(current_email).to have_subject "Confirmation instructions"
-    expect(current_email).to have_body_text(user.full_name)
+    expect(current_email).to have_body_text(registered_user.full_name)
   end
 
   scenario "Visitor confirms account" do
-    open_email(user.email)
+    open_email(registered_user.email)
     visit_in_email "Confirm my account"
 
     expect(page).to have_content("Your email address has been successfully confirmed")
-    expect(page).to have_text(user.email)
+    expect(page).to have_text(registered_user.email)
   end
 end
