894734134c3674f29a9eb1a640f26f0da78bfc8f
Timur Vafin <timur.vafin@flatstack.com>
2015-06-22 18:45:25 +0300

    We need only to implement #to_model on decorated object

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -3,8 +3,7 @@ class BasePresenter
   private :record
 
   # active_model
-  delegate :to_model, :to_key, :to_param, :to_partial_path, :errors,
-    to: :record
+  delegate :to_model, to: :record
 
   def initialize(record)
     @record = record
