4fb1fb0276f09f9ae5a103c25fb1b48759fbbca1
Vasily Polovnyov <vasily@polovnyov.ru>
2015-06-24 18:48:43 +0300

    rip off `#sign_in` helper:
    * in most applications feature specs will rely on _authenticated_ user.
      For that case it's better to stub auth process with Warden's `login_as`
      instead of repeating full sign in process (open page, fill form, submit).
    * feature specs testing sign in logic use inlined version of `sign_in` helper.

--- spec/features/user/account/cancel_spec.rb
+++ spec/features/user/account/cancel_spec.rb
@@ -14,8 +14,9 @@ feature "Cancel Account" do
     expect(page).to have_content("Sign in")
     expect(page).to have_content("Bye! Your account has been successfully cancelled. We hope to see you again soon.")
 
-    visit new_user_session_path
-    sign_in(user.email, user.password)
+    click_link "Sign in"
+    fill_form(:user, user.attributes.slice(:email, :password))
+    click_button "Sign in"
 
     expect(page).to have_content("Invalid email or password.")
   end
--- spec/features/visitor/sign_in_spec.rb
+++ spec/features/visitor/sign_in_spec.rb
@@ -4,6 +4,13 @@ feature "Sign In" do
   let(:user) { create :user, :confirmed }
   let(:unconfirmed_user) { create :user, :not_confirmed }
 
+  def sign_in(email, password)
+    visit new_user_session_path
+
+    fill_form(:user, email: email, password: password)
+    click_button "Sign in"
+  end
+
   scenario "Visitor signs in with valid credentials" do
     sign_in(user.email, user.password)
 
