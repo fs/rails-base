c8f50d1984237ebc30cc0272cd182e172ff70a7b
Timur Vafin <timur.vafin@flatstack.com>
2015-08-19 20:30:44 +0300

    Add Faker to generate more real-world factories.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
   factory :user do
     email
-
-    full_name "John Smith"
+    full_name { Faker::Name.name }
     password "123456"
     password_confirmation { password }
   end
