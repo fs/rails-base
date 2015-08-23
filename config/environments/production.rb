547296f5a675959d0055aef02babfe4c0e7aa196
Timur Vafin <timur.vafin@flatstack.com>
2015-08-23 16:04:37 +0300

    Make ASSET_HOST optional
    In development mode `config.action_controller.asset_host` could be skipped,
    so no need to setup `ASSET_HOST` as `localhost:500`.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV.fetch("ASSET_HOST")
+  config.action_controller.asset_host = ENV["ASSET_HOST"] if ENV["ASSET_HOST"]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
