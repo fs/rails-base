e82fef752b7daa27838d32aaeff72ee3c5bcc816
Timur Vafin <timur.vafin@flatstack.com>
2015-10-14 16:46:40 +0300

    Move Rack::MiniProfiler authorization to initializer

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -1,1 +1,8 @@
+mini_profiler_ip_whitelist = ENV.fetch("MINI_PROFILER_IP_WHITELIST",  "").split(",")
+
+Rack::MiniProfiler.config.pre_authorize_cb = lambda { |env|
+  request = Rack::Request.new(env)
+  mini_profiler_ip_whitelist.include?(request.ip)
+}
+
 Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
