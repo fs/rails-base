14072010f06a6779bf7823569df691e167c16058
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2015-10-30 22:46:56 +0300

    rework oauth_organizer and fetch_oauth_user classes

--- app/interactors/oauth_organizer.rb
+++ app/interactors/oauth_organizer.rb
@@ -18,7 +18,23 @@ class OauthOrganizer
   private
 
   def user
-    @user ||= current_user || FetchOauthUser.new(auth).call
+    @user ||= current_user || fetch_oauth_user || build_user
+  end
+
+  def fetch_oauth_user
+    FetchOauthUser.new(auth).call if auth_verified?
+  end
+
+  def auth_verified?
+    AuthVerificationPolicy.verified?(auth)
+  end
+
+  def build_user
+    User.build_from_omniauth(auth) if auth_verified? && !found_user_by_email?
+  end
+
+  def found_user_by_email?
+    User.find_by(email: auth["info"]["email"]).present?
   end
 
   def fail_oauth
--- spec/interactors/fetch_oauth_user_spec.rb
+++ spec/interactors/fetch_oauth_user_spec.rb
@@ -7,49 +7,21 @@ describe FetchOauthUser do
 
   subject { service.call }
 
-  before do
-    allow(AuthVerificationPolicy).to receive(:verified?).and_return(verified)
-  end
-
   context "when user is persisted" do
     let!(:user) { create(:user, user_attributes) }
 
-    context "when auth verified" do
-      let(:verified) { true }
-
-      it { is_expected.to eq(user) }
-    end
-
-    context "when auth is not verified" do
-      let(:verified) { false }
-
-      it { is_expected.not_to eq(user) }
-    end
+    it { is_expected.to eq(user) }
   end
 
   context "when social_profile persisted" do
     let!(:social_profile) { create(:social_profile, user: user) }
 
     let(:user) { create(:user, user_attributes) }
-    let(:verified) { false }
 
     it { is_expected.to eq(social_profile.user) }
   end
 
   context "when user and social profile are not persisted" do
-    context "when auth is verified" do
-      let(:verified) { true }
-
-      it "returns new user" do
-        expect(subject).to be_a_new_record
-        expect(subject).to be_kind_of(User)
-      end
-    end
-
-    context "when auth is not verified" do
-      let(:verified) { false }
-
-      it { is_expected.to eq(nil) }
-    end
+    it { is_expected.to eq(nil) }
   end
 end
