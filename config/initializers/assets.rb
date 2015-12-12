ef9be44be0729b33b005abbb4865ade4f66f203e
Kirill Kayumov <kirill.kayumov@flatstack.com>
2015-12-12 12:05:19 +0300

    Specify prefix of assets path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,6 @@ Rails.application.config.assets.version = ENV.fetch("ASSETS_VERSION", "1.0")
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( vendor/modernizr.js )
+
+# Prefix of assets path
+Rails.application.config.assets.prefix = "/assets"
