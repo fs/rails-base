b6d918240fc91f9a740e6a40abf6b6f5e7ff7adc
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2015-12-26 23:40:16 +0300

    use shared_examples

--- spec/features/visitor/sign_in_with_social_spec.rb
+++ spec/features/visitor/sign_in_with_social_spec.rb
@@ -24,21 +24,10 @@ feature "Sign in with social account" do
       context "when user not confirmed" do
         let!(:user) { create(:user, :from_auth_hashie) }
 
-        scenario "Users should finish registration" do
-          expect(page).to have_text("Finish Signup")
-
-          fill_in :user_email, with: "mailer@mail.com"
-          fill_in :user_password, with: "123456qwe"
-          click_button "Finish Signup"
-
-          open_email("mailer@mail.com")
-          expect(current_email).to have_subject("Confirmation instructions")
-          expect(current_email).to have_body_text(user.full_name)
-
-          visit_in_email("Confirm my account")
-          expect(page).to have_content("Your email address has been successfully confirmed")
-          expect(page).to have_text(user.full_name)
-          expect(page).to have_text("mailer@mail.com")
+        include_examples "finishing_sign_up" do
+          let(:name) { user.full_name }
+          let(:email) { "mailer@mail.com" }
+          let(:password) { "123456qwe" }
         end
       end
     end
@@ -61,53 +50,19 @@ feature "Sign in with social account" do
       context "when user not confirmed" do
         let!(:user) { create(:user, :from_auth_hashie) }
 
-        before do
-          visit new_user_session_path
-          click_link "Sign in with Facebook"
-        end
-
-        scenario "Users should finish registration" do
-          expect(page).to have_text("Finish Signup")
-
-          fill_in :user_email, with: "mailer@mail.com"
-          fill_in :user_password, with: "123456qwe"
-          click_button "Finish Signup"
-
-          open_email("mailer@mail.com")
-          expect(current_email).to have_subject("Confirmation instructions")
-          expect(current_email).to have_body_text(user.full_name)
-
-          visit_in_email("Confirm my account")
-          expect(page).to have_content("Your email address has been successfully confirmed")
-          expect(page).to have_text(user.full_name)
-          expect(page).to have_text("mailer@mail.com")
+        include_examples "finishing_sign_up" do
+          let(:name) { user.full_name }
+          let(:email) { "mailer@mail.com" }
+          let(:password) { "123456qwe" }
         end
       end
     end
 
     context "when user not found" do
-      let(:name_from_auth_hash) { "Joe Bloggs" }
-
-      before do
-        visit new_user_session_path
-        click_link "Sign in with Facebook"
-      end
-
-      scenario "Users should finish registration" do
-        expect(page).to have_text("Finish Signup")
-
-        fill_in :user_email, with: "mailer@mail.com"
-        fill_in :user_password, with: "123456qwe"
-        click_button "Finish Signup"
-
-        open_email("mailer@mail.com")
-        expect(current_email).to have_subject("Confirmation instructions")
-        expect(current_email).to have_body_text(name_from_auth_hash)
-
-        visit_in_email("Confirm my account")
-        expect(page).to have_content("Your email address has been successfully confirmed")
-        expect(page).to have_text(name_from_auth_hash)
-        expect(page).to have_text("mailer@mail.com")
+      include_examples "finishing_sign_up" do
+        let(:name) { "Joe Bloggs" }
+        let(:email) { "mailer@mail.com" }
+        let(:password) { "123456qwe" }
       end
     end
   end
@@ -144,28 +99,10 @@ feature "Sign in with social account" do
     end
 
     context "when user not found" do
-      let(:name_from_auth_hash) { "Joe Bloggs" }
-
-      before do
-        visit new_user_session_path
-        click_link "Sign in with Facebook"
-      end
-
-      scenario "Users connects social profile" do
-        expect(page).to have_text("Finish Signup")
-
-        fill_in :user_email, with: "mailer@mail.com"
-        fill_in :user_password, with: "123456qwe"
-        click_button "Finish Signup"
-
-        open_email("mailer@mail.com")
-        expect(current_email).to have_subject("Confirmation instructions")
-        expect(current_email).to have_body_text(name_from_auth_hash)
-
-        visit_in_email("Confirm my account")
-        expect(page).to have_content("Your email address has been successfully confirmed")
-        expect(page).to have_text(name_from_auth_hash)
-        expect(page).to have_text("mailer@mail.com")
+      include_examples "finishing_sign_up" do
+        let(:name) { "Joe Bloggs" }
+        let(:email) { "mailer@mail.com" }
+        let(:password) { "123456qwe" }
       end
     end
   end
---
+++ spec/support/shared_examples/finishing_sign_up.rb
@@ -0,0 +1,23 @@
+RSpec.shared_examples "finishing_sign_up" do
+  before do
+    visit new_user_session_path
+    click_link "Sign in with Facebook"
+  end
+
+  scenario "User finishing registration" do
+    expect(page).to have_text("Finish Signup")
+
+    fill_in :user_email, with: email
+    fill_in :user_password, with: password
+    click_button "Finish Signup"
+
+    open_email("mailer@mail.com")
+    expect(current_email).to have_subject("Confirmation instructions")
+    expect(current_email).to have_body_text(name)
+
+    visit_in_email("Confirm my account")
+    expect(page).to have_content("Your email address has been successfully confirmed")
+    expect(page).to have_text(name)
+    expect(page).to have_text("mailer@mail.com")
+  end
+end
