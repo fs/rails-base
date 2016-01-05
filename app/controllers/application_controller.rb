7042f6b299d578e1026dc09a39cd4d0b1f6b05a2
Timur Vafin <timur.vafin@flatstack.com>
2016-01-05 16:12:58 +0300

    Respond to HTML by default
    Closes #387

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   include Authorization
 
   protect_from_forgery with: :exception
+
   responders :flash
+  respond_to :html
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
