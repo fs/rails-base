06899bcd3c7b99e4c4dc1cba4681cd723c2ab958
Slava Kisel <slavakisel@users.noreply.github.com>
2016-04-22 13:35:02 +0300

    Allow users to edit their account without providing a current password (#442)

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,9 @@
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    protected
+
+    def update_resource(resource, params)
+      resource.update_without_password(params)
+    end
+  end
+end
