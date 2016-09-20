1a63ef8f2df24f3f19bb8feba7fc4f39f45e4b10
Kirill Kayumov <kirill.kayumov@flatstack.com>
2016-09-20 23:59:53 +0300

    Fix password updating

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,21 @@ module Users
     protected
 
     def update_resource(resource, params)
-      resource.update_without_password(params)
+      resource.update_attributes(params)
+    end
+
+    def account_update_params
+      params = devise_parameter_sanitizer.sanitize(:account_update)
+
+      if passwords_blank?(params)
+        params.except(:password, :password_confirmation)
+      else
+        params
+      end
+    end
+
+    def passwords_blank?(params)
+      params[:password].blank? && params[:password_confirmation].blank?
     end
   end
 end
