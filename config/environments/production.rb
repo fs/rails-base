6179fc97952de9f4c654d773344dfbd46a365cd9
Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>
2017-02-21 15:04:21 +0300

    correct rails configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,9 +32,6 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV["ASSET_HOST"] if ENV["ASSET_HOST"]
-
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
@@ -60,6 +57,9 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  config.action_controller.asset_host = ENV["ASSET_HOST"] if ENV["ASSET_HOST"]
 
   config.action_mailer.perform_caching = false
 
