672096015dda0544235cb40aa966ba62598977ca
Eduard Ryazapov <eduard.ryazapov@flatstack.com>
2017-11-27 19:36:24 +0300

    add window config

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,5 +1,9 @@
 require "capybara/poltergeist"
 
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, window_size: %i[1440 1280])
+end
+
 Capybara.configure do |config|
   config.match = :prefer_exact
   config.javascript_driver = :poltergeist
