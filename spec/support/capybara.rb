3f9a4c69274b24b5a9c76a1da2a305e6c51589a3
Eduard Ryazapov <eduard.ryazapov@flatstack.com>
2017-11-28 17:22:08 +0300

    add settings for driver

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,7 +1,22 @@
-require "capybara/poltergeist"
+require "selenium/webdriver"
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: {
+      args: %w[headless disable-gpu window-size=1440,1280]
+    }
+  )
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+end
 
 Capybara.configure do |config|
   config.match = :prefer_exact
-  config.javascript_driver = :poltergeist
+  config.javascript_driver = :headless_chrome
+  config.default_max_wait_time = 5
   config.asset_host = "http://#{ENV.fetch('HOST')}"
 end
