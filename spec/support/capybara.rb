0909acac3fa850388843d2e4545b402acdbcf212
Eduard Ryazapov <eduard.ryazapov@flatstack.com>
2017-12-18 17:08:10 +0300

    update capybara

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,9 +1,5 @@
 require "selenium/webdriver"
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
     chromeOptions: {
