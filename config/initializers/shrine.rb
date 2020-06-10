58fceb2ec530141747d60d576d965967e95ffcdc
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2020-06-10 19:45:32 +0300

    WIP

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,6 +1,7 @@
 require "shrine"
 require "shrine/storage/file_system"
 require "shrine/storage/s3"
+require "shrine/storage/memory"
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data
@@ -12,10 +13,10 @@ Shrine.plugin :validation_helpers
 
 def s3_options
   {
-    access_key_id: ENV["S3_ACCESS_KEY_ID"],
+    access_key_id:     ENV["S3_ACCESS_KEY_ID"],
     secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
-    region: ENV["S3_BUCKET_REGION"],
-    bucket: ENV["S3_BUCKET_NAME"]
+    region:            ENV["S3_BUCKET_REGION"],
+    bucket:            ENV["S3_BUCKET_NAME"]
   }
 end
 
@@ -27,10 +28,17 @@ def amazon_s3_storages
 end
 
 def local_storages
-  {
-    cache: Shrine::Storage::FileSystem.new("public", prefix: "/uploads/#{Rails.env}/cache"),
-    store: Shrine::Storage::FileSystem.new("public", prefix: "/uploads/#{Rails.env}")
-  }
+  if Rails.env.test?
+    {
+      cache: Shrine::Storage::Memory.new,
+      store: Shrine::Storage::Memory.new
+    }
+  else
+    {
+      cache: Shrine::Storage::FileSystem.new("public", prefix: "/uploads/cache"),
+      store: Shrine::Storage::FileSystem.new("public", prefix: "/uploads")
+    }
+  end
 end
 
 Shrine.storages = s3_options.values.all? ? amazon_s3_storages : local_storages
