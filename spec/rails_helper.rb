13bbc15902e0ebf6a331971b542de912c13f4ab3
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2020-06-16 20:02:58 +0300

    Test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,6 @@ require "shoulda/matchers"
 Dir[Rails.root.join("spec", "support", "**", "*.rb")].sort.each { |f| require f }
 
 RSpec.configure do |config|
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
 end
