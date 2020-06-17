f5c4fe84633966d732ec0828e6e8baac48554af8
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2020-06-17 12:49:22 +0300

    Styles and JS for tests

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w[mailer jasmine-jquery.js]
+Rails.application.config.assets.precompile += %w[mailer styles_for_tests.css jasmine-jquery.js]
 
 # Unknown asset fallback will return the path passed in when the given
 # asset is not present in the asset pipeline.
