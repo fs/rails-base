2f35cc621b595878d717e61943b58de3c7a4dd76
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2020-07-14 15:36:04 +0300

    Added rack-cors gem and initial settings (#595)
    * Added rack-cors gem and initial settings

---
+++ config/initializers/cors.rb
@@ -0,0 +1,16 @@
+# Be sure to restart your server when you modify this file.
+
+# Avoid CORS issues when API is called from the frontend app.
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+
+# Read more: https://github.com/cyu/rack-cors
+
+if ENV["CORS_ORIGINS"].present?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
+    allow do
+      origins(*ENV.fetch("CORS_ORIGINS").split(","))
+
+      resource "/assets/*", headers: :any, methods: :get
+    end
+  end
+end
