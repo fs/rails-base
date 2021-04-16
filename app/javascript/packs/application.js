a7d39e9ec98c7e29562b5bffd1f33492d020cf2c
Dmitry Barskov <dmitry.barskov@flatstack.com>
2021-04-16 16:58:21 +0300

    Remove react from the project
    Co-authored-by: Evgeniy Esaulkov <evgeniy.esaulkov@flatstack.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,2 @@
-/* eslint no-console:0 */
-import User from "../classes/user"
-
-require("jquery")
-require("@rails/ujs").start()
-
-window.App = {}
-
-App.currentUserData = JSON.parse(document.getElementsByTagName('body')[0].getAttribute('data-current-user'))
-
-if (App.currentUserData) {
-  App.currentUser = new User(App.currentUserData);
-}
+require("jquery");
+require("@rails/ujs").start();
